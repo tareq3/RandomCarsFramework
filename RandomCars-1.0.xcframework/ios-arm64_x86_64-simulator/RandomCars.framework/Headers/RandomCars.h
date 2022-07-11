a829e9f22da31146b98ffbfe42482dd3d3dcd62b
Tareq <tareq@threls.com>
2022-07-12 01:19:35 +0600

    initial framework added

---
+++ RandomCars-1.0.xcframework/ios-arm64_x86_64-simulator/RandomCars.framework/Headers/RandomCars.h
@@ -0,0 +1,18 @@
+//
+//  RandomCars.h
+//  RandomCars
+//
+//  Created by Tareq Islam on 12/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RandomCars.
+FOUNDATION_EXPORT double RandomCarsVersionNumber;
+
+//! Project version string for RandomCars.
+FOUNDATION_EXPORT const unsigned char RandomCarsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomCars/PublicHeader.h>
+
+
